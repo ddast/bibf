6071ea4db2ede239444aeab5b5b7d6600ed8daea
Dennis Dast <mail@ddast.de>
2014-05-31 12:18:09 +0200

    Added function to header file

--- src/bibf.hpp
+++ src/bibf.hpp
@@ -21,6 +21,9 @@
 #ifndef BIBF_H
 #define BIBF_H
 
+// Separate comma separated list of words into a vector of words
+std::vector<std::string> separate_string(std::string s)
+
 // Converts a string with comma separated parts into a vector
 std::vector<std::string> separate_string(std::string s);
 
