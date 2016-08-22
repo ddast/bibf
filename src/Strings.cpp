7035dcbf89ea252366df6b2afd4cae050c61c2ad
Dennis Dast <mail@ddast.de>
2016-08-22 15:38:15 +0200

    Bump version to 0.1.9

--- src/Strings.cpp
+++ src/Strings.cpp
@@ -21,7 +21,7 @@
 #include "Strings.hpp"
 
 // program version
-const std::string Strings::version = "0.1.8";
+const std::string Strings::version = "0.1.9";
 
 // Standard value of current_lang
 Strings::LANG Strings::current_lang = Strings::LANG_EN;
